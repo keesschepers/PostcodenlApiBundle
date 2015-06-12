da8f24c26534c73186a3401f7bfe3552c88dd851
Kees Schepers <kees@keesschepers.nl>
2015-06-12 21:50:20 +0200

    Prevent from an error being logged in case of 404.

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -5,16 +5,23 @@ namespace Keesschepers\PostcodenlApiBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Guzzle\Http\Exception\ClientErrorResponseException;
 
 class DefaultController extends Controller
 {
     public function indexAction(Request $request)
     {
         $api = $this->get('keesschepers_postcodenl_api.api');
-        $response = $api->getResponseByPostcodeAndHousenumber(
-                $request->query->get('zipcode'),
-                $request->query->get('address-number')
-        );
+
+        try {
+            $response = $api->getResponseByPostcodeAndHousenumber(
+                    $request->query->get('zipcode'),
+                    $request->query->get('address-number')
+            );
+        } catch (ClientErrorResponseException $e) {
+            return new Response('The given zipcode and address number didnt result in a address.', 400);
+        }
 
         return new JsonResponse($response);
     }
